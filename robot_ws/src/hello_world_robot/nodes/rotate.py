3a1472ab363530607f2d7aa9fdbfeffae3b01733
Kian Dadkhah <61067804+techapostle@users.noreply.github.com>
2022-02-05 20:06:33 -0500

    robot stops moving after a while

--- robot_ws/src/hello_world_robot/nodes/rotate.py
+++ robot_ws/src/hello_world_robot/nodes/rotate.py
@@ -16,32 +16,45 @@
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
-from geometry_msgs.msg import Twist
-
+from geometry_msgs.msg import Twist, Pose, Point, Quaternion
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from actionlib_msgs.msg import *
+import actionlib
 import rospy
 
 
 class Rotator():
 
+
+
     def __init__(self):
         self._cmd_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+    
+    def move_forward(self):
+        keepGoing = 0
 
-    def rotate_forever(self):
         self.twist = Twist()
 
         r = rospy.Rate(10)
-        while not rospy.is_shutdown():
-            self.twist.angular.z = 0.1
-            self._cmd_pub.publish(self.twist)
-            rospy.loginfo('Rotating robot: %s', self.twist)
+        while not rospy.is_shutdown(): 
+            if not keepGoing > 3:
+                self.twist.angular.z = 0.6
+            elif not keepGoing > 12:
+                self.twist.linear.x = 0.4
+                self.twist.angular.z = 0.0
+
+            rospy.loginfo('Moving robot: %s', self.twist)
             r.sleep()
+            self._cmd_pub.publish(self.twist)
+            keepGoing += 1
+                  
 
 
 def main():
     rospy.init_node('rotate')
     try:
-        rotator = Rotator()
-        rotator.rotate_forever()
+        mover = Rotator()
+        mover.move_forward()
     except rospy.ROSInterruptException:
         pass
 
